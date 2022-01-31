08a42019ef755143ba2b64d27c6c1ecaea9d8ba5
Daniel Stahl <danstahl1138@gmail.com>
2022-01-31 11:47:12 +0000

    added okta react

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import JobsPerProject from './components/JobsPerProject';
 import {
@@ -12,10 +10,23 @@ import {
 } from "@apollo/client";
 import { setContext } from '@apollo/client/link/context';
 
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { Security, SecureRoute, LoginCallback } from '@okta/okta-react';
+import { OktaAuth } from '@okta/okta-auth-js';
+
+const issuer = process.env.REACT_APP_OKTA_URL_BASE + '/oauth2/default'
+const clientId = process.env.REACT_APP_OKTA_CLIENTID;
+const redirect = process.env.REACT_APP_OKTA_APP_BASE_URL + '/callback';
+
+
 const httpLink = createHttpLink({
   uri: process.env.REACT_GRAPHQL_URL,
 });
-
+const oktaAuth = new OktaAuth({
+  issuer: issuer,
+  clientId: clientId,
+  redirectUri: redirect
+});  
 const authLink = setContext((_, { headers }) => {
   // get the authentication token from local storage if it exists
   const token = localStorage.getItem('token'); //TODO, need a full login process with okta etc
@@ -31,13 +42,20 @@ const client = new ApolloClient({
   link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 });
+const Home=()=><div>Hello world</div>
+const App=()=>(
+  <ApolloProvider client={client}>
+    <Security oktaAuth={oktaAuth}>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} ></Route>
+        <SecureRoute path='/table' element={<JobsPerProject/>} />
+        <Route path='/callback' element={<LoginCallback/>} />
+      </Routes>
+      </BrowserRouter>
+      
+    </Security>
+    </ApolloProvider>
+  )
 
-function App() {
-  return (
-    <div className="App">
-      <JobsPerProject />
-    </div>
-  );
-}
-
-export default <ApolloProvider client={client}><App /></ApolloProvider>;
+export default App;
